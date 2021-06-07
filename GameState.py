7d3b0c3d12d9cbca48565d2fbc608f9855e092bf
Heba Elwazzan <hebaelwazan@gmail.com>
2021-06-07 22:45:20 +0200

    Added isWinning and put matching fours in a separate function to evaluate final score

--- GameState.py
+++ GameState.py
@@ -7,6 +7,16 @@ PLAYERS = [AI_PLAYER, HUMAN_PLAYER]
 TWO_IN_A_ROW = 4
 THREE_IN_A_ROW = 12
 FOUR_IN_A_ROW = 100
+positiveDiagonalIndices = [0, 1, 2, 6, 7, 8, 12, 13, 14, 18, 19, 20]
+negativeDiagonalIndices = [3, 4, 5, 9, 10, 11, 15, 16, 17, 21, 22, 23]
+positiveDiagonalFor3 = copy.copy(positiveDiagonalIndices)
+positiveDiagonalFor3.extend([9, 15, 21, 25, 26, 27])
+negativeDiagonalFor3 = copy.copy(negativeDiagonalIndices)
+negativeDiagonalFor3.extend([8, 14, 20, 26, 27, 28])
+positiveDiagonalFor2 = copy.copy(positiveDiagonalFor3)
+positiveDiagonalFor2.extend([16, 22, 28, 32, 33, 34])
+negativeDiagonalFor2 = copy.copy(negativeDiagonalFor3)
+negativeDiagonalFor2.extend([13, 19, 25, 31, 32, 33])
 
 NUM_COLUMNS = 7
 
@@ -73,16 +83,6 @@ class GameState:
 
         score = 0
         currGrid = self.grid
-        positiveDiagonalIndices = [0, 1, 2, 6, 7, 8, 12, 13, 14, 18, 19, 20]
-        negativeDiagonalIndices = [3, 4, 5, 9, 10, 11, 15, 16, 17, 21, 22, 23]
-        positiveDiagonalFor3 = copy.copy(positiveDiagonalIndices)
-        positiveDiagonalFor3.extend([9, 15, 21, 25, 26, 27])
-        negativeDiagonalFor3 = copy.copy(negativeDiagonalIndices)
-        negativeDiagonalFor3.extend([8, 14, 20, 26, 27, 28])
-        positiveDiagonalFor2 = copy.copy(positiveDiagonalFor3)
-        positiveDiagonalFor2.extend([16, 22, 28, 32, 33, 34])
-        negativeDiagonalFor2 = copy.copy(negativeDiagonalFor3)
-        negativeDiagonalFor2.extend([13, 19, 25, 31, 32, 33])
 
         # 2 in a row:
         # 1. HORIZONTALLY:
@@ -147,41 +147,8 @@ class GameState:
                     and currGrid[i + 10] == HUMAN_PLAYER:
                 score -= THREE_IN_A_ROW
         # 4 in a row
-        # 1. HORIZONTALLY:
-        for i in range(23):
-            if currGrid[i] == AI_PLAYER and currGrid[i + 6] == AI_PLAYER and currGrid[i + 12] == AI_PLAYER \
-                    and currGrid[i + 18] == AI_PLAYER:
-                score += FOUR_IN_A_ROW
-            elif currGrid[i] == HUMAN_PLAYER and currGrid[i + 6] == HUMAN_PLAYER and currGrid[i + 12] == HUMAN_PLAYER \
-                    and currGrid[i + 18] == HUMAN_PLAYER:
-                score -= FOUR_IN_A_ROW
-        # 2. Vertically
-        for j in range(0, 36, 6):
-            for i in range(4):
-                if currGrid[i + j] == AI_PLAYER and currGrid[i + j + 1] == AI_PLAYER and currGrid[
-                    i + j + 2] == AI_PLAYER \
-                        and currGrid[i + j + 3] == AI_PLAYER:
-                    score += FOUR_IN_A_ROW
-                elif currGrid[i + j] == HUMAN_PLAYER and currGrid[i + j + 1] == HUMAN_PLAYER \
-                        and currGrid[i + j + 2] == HUMAN_PLAYER and currGrid[i + j + 3] == HUMAN_PLAYER:
-                    score -= FOUR_IN_A_ROW
-        # 3. Positively sloped diagonals:
-        # (don't consider the cases where the diagonal length is less than 4 as it is meaningless)
-        for i in positiveDiagonalIndices:
-            if currGrid[i] == AI_PLAYER and currGrid[i + 7] == AI_PLAYER and currGrid[i + 14] == AI_PLAYER \
-                    and currGrid[i + 21] == AI_PLAYER:
-                score += FOUR_IN_A_ROW
-            elif currGrid[i] == HUMAN_PLAYER and currGrid[i + 7] == HUMAN_PLAYER \
-                    and currGrid[i + 14] == HUMAN_PLAYER and currGrid[i + 21] == HUMAN_PLAYER:
-                score -= FOUR_IN_A_ROW
-        # 4. Negatively sloped diagonals:
-        for i in negativeDiagonalIndices:
-            if currGrid[i] == AI_PLAYER and currGrid[i + 5] == AI_PLAYER and currGrid[i + 10] == AI_PLAYER \
-                    and currGrid[i + 15] == AI_PLAYER:
-                score += FOUR_IN_A_ROW
-            elif currGrid[i] == HUMAN_PLAYER and currGrid[i + 5] == HUMAN_PLAYER \
-                    and currGrid[i + 10] == HUMAN_PLAYER and currGrid[i + 15] == HUMAN_PLAYER:
-                score -= FOUR_IN_A_ROW
+        score = self.countMatchingFours(score)
+
         # A piece on the center left or right
         for i in range(12, 18):
             if currGrid[i] == AI_PLAYER:
@@ -220,6 +187,45 @@ class GameState:
 
         return score
 
+    def countMatchingFours(self, score):
+        currGrid = self.grid
+        # 1. HORIZONTALLY:
+        for i in range(23):
+            if currGrid[i] == AI_PLAYER and currGrid[i + 6] == AI_PLAYER and currGrid[i + 12] == AI_PLAYER \
+                    and currGrid[i + 18] == AI_PLAYER:
+                score += FOUR_IN_A_ROW
+            elif currGrid[i] == HUMAN_PLAYER and currGrid[i + 6] == HUMAN_PLAYER and currGrid[i + 12] == HUMAN_PLAYER \
+                    and currGrid[i + 18] == HUMAN_PLAYER:
+                score -= FOUR_IN_A_ROW
+        # 2. Vertically
+        for j in range(0, 36, 6):
+            for i in range(4):
+                if currGrid[i + j] == AI_PLAYER and currGrid[i + j + 1] == AI_PLAYER and currGrid[
+                    i + j + 2] == AI_PLAYER \
+                        and currGrid[i + j + 3] == AI_PLAYER:
+                    score += FOUR_IN_A_ROW
+                elif currGrid[i + j] == HUMAN_PLAYER and currGrid[i + j + 1] == HUMAN_PLAYER \
+                        and currGrid[i + j + 2] == HUMAN_PLAYER and currGrid[i + j + 3] == HUMAN_PLAYER:
+                    score -= FOUR_IN_A_ROW
+        # 3. Positively sloped diagonals:
+        # (don't consider the cases where the diagonal length is less than 4 as it is meaningless)
+        for i in positiveDiagonalIndices:
+            if currGrid[i] == AI_PLAYER and currGrid[i + 7] == AI_PLAYER and currGrid[i + 14] == AI_PLAYER \
+                    and currGrid[i + 21] == AI_PLAYER:
+                score += FOUR_IN_A_ROW
+            elif currGrid[i] == HUMAN_PLAYER and currGrid[i + 7] == HUMAN_PLAYER \
+                    and currGrid[i + 14] == HUMAN_PLAYER and currGrid[i + 21] == HUMAN_PLAYER:
+                score -= FOUR_IN_A_ROW
+        # 4. Negatively sloped diagonals:
+        for i in negativeDiagonalIndices:
+            if currGrid[i] == AI_PLAYER and currGrid[i + 5] == AI_PLAYER and currGrid[i + 10] == AI_PLAYER \
+                    and currGrid[i + 15] == AI_PLAYER:
+                score += FOUR_IN_A_ROW
+            elif currGrid[i] == HUMAN_PLAYER and currGrid[i + 5] == HUMAN_PLAYER \
+                    and currGrid[i + 10] == HUMAN_PLAYER and currGrid[i + 15] == HUMAN_PLAYER:
+                score -= FOUR_IN_A_ROW
+        return score
+
     def isTerminal(self):
         """
         returns boolean that indicates if the board is completely filled or not
@@ -231,18 +237,22 @@ class GameState:
                 return True
 
     def isWinning(self, player):
-        """
-        returns a boolean to indicate if state is winning for current player or not
-        """
-        pass
+        if self.isTerminal():
+            score = self.countMatchingFours(0) // 100
+            print("Final score is " + str(score))
+            if (score > 0 and player == AI_PLAYER) or (score < 0 and player == HUMAN_PLAYER):
+                return True
+        return False  # other player is winning or draw or not terminal yet
 
-
-def printGrid(grid):
-    for i in range(6):
-        for x in range(7):
-            print(grid[x * 6 + 5 - i], end=" ")
+    def printGrid(self):
+        for i in range(6):
+            for x in range(7):
+                print(self.grid[x * 6 + 5 - i], end=" ")
+            print()
         print()
-    print()
+
+
+
 
 
 # newGrid = "0" * 42
@@ -293,5 +303,5 @@ def printGrid(grid):
 
 grid = "200000100000210000000000110000122200000000"
 gameState = GameState(grid, random.choice(PLAYERS), None)
-printGrid(gameState.grid)
+gameState.printGrid()
 print(gameState.evalState())
