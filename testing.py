859a740a8c28bdeae6e075a7846021e4a45e3a1f
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-06-12 18:22:51 +0200

    Change font sizes

--- testing.py
+++ testing.py
@@ -68,7 +68,15 @@ for k in k_alphabeta:
 print('Done')
 
 #%%
-figure = plt.figure(figsize=(12,12))
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+figure = plt.figure(figsize=(16,10))
 plt.style.use('fivethirtyeight')
 plt.plot(k_minimax, data_points_minimax, label='No Pruning')
 plt.plot(k_alphabeta, data_points_ab, label='With Pruning')
